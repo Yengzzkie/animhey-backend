0e71f0214c49da0e31932c3d7b28b160988f9ac7
Yengzzkie <gatchalian.manuel@gmail.com>
2024-06-22 14:44:07 -0400

    changed endpoint

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const visitsRoute = require('./api/visits');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +13,12 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use(express.json());
 app.use('/api/visits', visitsRoute);
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
