cd2801f7d05c87e649b4eeecf94bd6ede3af9762
Yengzzkie <gatchalian.manuel@gmail.com>
2024-06-22 18:37:49 -0400

    enable CORS access

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 app.use('/api/visits', visitsRoute);
-
-app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
